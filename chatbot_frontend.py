bb573ba4d05b6820fe55401ee81de28499bac984
eshamalik080 <eshamalik5571@gmail.com>
2026-09-14 15:07:35 +0500

    Added Streamlit frontend

---
+++ chatbot_frontend.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from chatbot_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+config = {'configurable': {'thread_id': 'thread_1'}}
+
+# st.session_state -> dict -> is ma pehla sa pari cheza erase nhi hoti
+if 'message_history' not in st.session_state:
+     st.session_state['message_history']  = []
+
+for msg in st.session_state['message_history']:
+    with st.chat_message(msg['role']):
+         st.text(msg['content'])
+
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=Hello'}
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    #first add msg to msg history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content= user_input)]}, config=config)
+    ai_message = response['messages'][-1].content
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+            st.text(ai_message)
+
+
+
+
+
